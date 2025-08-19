09e44f81f5ba44bb66c850210d879b4b30f1f4b7
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2025-08-20 00:05:12 +0530

    resume analyzer home page created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications and Reasume Ratings</h1>
+          <h2>Review Your Submissions and Check AI Powered Feedback</h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div>
+          {resumes.map((resume) => (
+            <div>
+              <ResumeCard key={resume.id} resume={resume} />
+            </div>
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
