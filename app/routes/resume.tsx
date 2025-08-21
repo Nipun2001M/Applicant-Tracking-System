f4097fdc782b370777e93240a6723ecdba19632d
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2025-08-22 00:29:47 +0530

    resume feedback page implimented 1/2 only

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summery from "~/components/Summery";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
@@ -8,46 +11,42 @@ export const meta = () => [
 ];
 
 const resume = () => {
-    const {auth,isLoading,fs,kv}=usePuterStore()
-    const { id } = useParams();
-    const [imageUrl,setImageURL]=useState('')
-    const [resumeUrl,setResumeURL]=useState('')
-    const [feedbackUrl,setFeedback]=useState('')
-    const navigate=useNavigate()
+  const { auth, isLoading, fs, kv } = usePuterStore();
+  const { id } = useParams();
+  const [imageUrl, setImageURL] = useState("");
+  const [resumeUrl, setResumeURL] = useState("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!isLoading && !auth.isAuthenticated)
+      navigate(`/auth?next=/resume/${id}`);
+  }, [auth.isAuthenticated]);
 
-  useEffect(()=>{
-    const loadResume=async()=>{
-        const resume=await kv.get(`resume:${id}`)
-        if(!resume) return
+  useEffect(() => {
+    const loadResume = async () => {
+      const resume = await kv.get(`resume:${id}`);
+      if (!resume) return;
 
-        const data=JSON.parse(resume)
-        //putr cloud returns blobs
-        const resumeBlob=await fs.read(data.resumePath)
-        if (!resumeBlob) return
+      const data = JSON.parse(resume);
+      //putr cloud returns blobs
+      const resumeBlob = await fs.read(data.resumePath);
+      if (!resumeBlob) return;
 
-        const pdfBlob=new Blob([resumeBlob],{type:"application/pdf"})
-        const resumeUrl=URL.createObjectURL(pdfBlob)
-        setResumeURL(resumeUrl)
+      const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+      const resumeUrl = URL.createObjectURL(pdfBlob);
+      setResumeURL(resumeUrl);
 
-        const imageBlob=await fs.read(data.imagePath)
-        if(!imageBlob) return
-        const imageUrl=URL.createObjectURL(imageBlob)
-        setImageURL(imageUrl)
+      const imageBlob = await fs.read(data.imagePath);
+      if (!imageBlob) return;
+      const imageUrl = URL.createObjectURL(imageBlob);
+      setImageURL(imageUrl);
 
-        setFeedback(data.feedback)
+      setFeedback(data.feedback);
+    };
 
-
-        
-
-
-
-    }
-
-    loadResume()
-
-
-  },[id])
+    loadResume();
+  }, [id]);
 
   return (
     <main className="!pt-0">
@@ -60,14 +59,35 @@ const resume = () => {
         </Link>
       </nav>
       <div className="flex flex-row w-full max-lg:flex-col-reverse">
+        <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+          {imageUrl && resumeUrl && (
+            <div className=" animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+              <a href={resumeUrl} target="_blank">
+                <img
+                  src={imageUrl}
+                  className="w-full h-full object-contain rounded-2xl"
+                  title="resume"
+                />
+              </a>
+            </div>
+          )}
+        </section>
         <section className="feedback-section">
-            {imageUrl && resumeUrl && (
-
-                
-
-
-            ) }
-
+          <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+          {feedback ? (
+            <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+              <Summery feedback={feedback} />
+              <ATS
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              />
+              <Details feedback={feedback} />
+            </div>
+          ) : (
+            <div>
+              <img src="/images/resume-scan-2.gif" className="w-full" />
+            </div>
+          )}
         </section>
       </div>
     </main>
