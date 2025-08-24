70fb38423ed4b229d3d62041754071315c6ad8e1
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2025-08-24 14:54:57 +0530

    resume image issue fixed

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -44,7 +44,7 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
       <div className="gradient-border animate-in fade-in duration-1000">
         <div className="w-full w-full">
           <img
-            src={resume.imagePath}
+            src={resumeUrl}
             alt="resume"
             className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
           />
